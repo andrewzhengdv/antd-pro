97fda1dd337155d64d6cef4b93fe221aa38f3665
Andrew Zheng <andrew.zheng@bcgdv.com>
2020-03-30 20:29:22 -0700

    first commit

---
+++ src/pages/list/table-list/components/InviteForm.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Form, Input, Modal } from 'antd';
+
+const FormItem = Form.Item;
+
+interface CreateFormProps {
+  modalVisible: boolean;
+  onSubmit: (fieldsValue: { desc: string }) => void;
+  onCancel: () => void;
+}
+
+const InviteForm: React.FC<CreateFormProps> = props => {
+  const [form] = Form.useForm();
+
+  const { modalVisible, onSubmit: handleAdd, onCancel } = props;
+  const okHandle = async () => {
+    const fieldsValue = await form.validateFields();
+    form.resetFields();
+    handleAdd(fieldsValue);
+  };
+  return (
+    <Modal
+      destroyOnClose
+      title="Invite"
+      visible={modalVisible}
+      onOk={okHandle}
+      onCancel={() => onCancel()}
+    >
+      <Form form={form}>
+        <FormItem
+          labelCol={{ span: 5 }}
+          wrapperCol={{ span: 15 }}
+          label="Email body"
+          name="desc"
+          rules={[{ required: true, message: 'email body here', min: 5 }]}
+        >
+          <Input placeholder="email body" />
+        </FormItem>
+      </Form>
+    </Modal>
+  );
+};
+
+export default InviteForm;
